65cb0c36a201211a399ac0b7c3203cd65fa86f34
Luzana <jaspercerwyn.luzana@tup.edu.ph>
2025-03-12 16:33:05 +0800

    First commit

---
+++ product-service.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.use(express.json());
+
+const products = [
+  { id: 1, name: 'Laptop' },
+  { id: 2, name: 'Phone' }
+]
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+})
+
+app.listen(PORT, () => {
+  console.log(`Product Service running on port ${PORT}`);
+})
